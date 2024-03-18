912f063567229f1ddf7f1b58fae4c6530794e5d6
csaz7716 <manuel.lahner@student.uibk.ac.at>
2024-03-18 18:57:33 +0100

    modified:   sheet2/solution/04-lahner.jl renamed:    sheet2/solution/sheet2.pdf -> sheet2/solution/lahner-written.pdf new file:   sheet2/solution/sheet2-lahner.zip

--- sheet2/solution/04-lahner.jl
+++ sheet2/solution/04-lahner.jl
@@ -4,29 +4,29 @@ function plot_function(fct, x_min, x_max, step_size)
     plot(x_min:step_size:x_max, map(fct, x_min:step_size:x_max))
 end
 
-f1(x) = 3x^2 - x - 7
-f1d = "f(x) = 3x^2 - x - 7"
-f1p = plot_function(f1, -5.0, 5.0, 0.01)
+fct_1(x) = 3x^2 - x - 7
+fct_1_str = "f(x) = 3x^2 - x - 7"
+fig_1 = plot_function(fct_1, -5.0, 5.0, 0.01)
 
-f2(x) = (7 / 5)^x - 0.5 * x^3
-f2d = "f(x) = (7/5)^x-0.5*x^3"
-f2p = plot_function(f2, -2.0, 2.0, 0.01)
+fct_2(x) = (7 / 5)^x - 0.5 * x^3
+fct_2_str = "f(x) = (7/5)^x-0.5*x^3"
+fig_2 = plot_function(fct_2, -2.0, 2.0, 0.01)
 
-f3(x) = 3 * sin(x) + cos(10 * x) * (1 / 3) * sin(x)
-f3d = "f(x) = 3sin(x)+cos(10x)*(1/3)*sin(x)"
-f3p = plot_function(f3, -6.0, 6.0, 0.01)
+fct_3(x) = 3 * sin(x) + cos(10 * x) * (1 / 3) * sin(x)
+fct_3_str = "f(x) = 3sin(x)+cos(10x)*(1/3)*sin(x)"
+fig_3 = plot_function(fct_3, -6.0, 6.0, 0.01)
 
-f4(x) = abs(abs(abs(abs(x) - 1) - 1) - 1)
-f4d = "f(x) = abs(abs(abs(abs(x) - 1) - 1) - 1)"
-f4p = plot_function(f4, -10.0, 10.0, 0.01)
+fct_4(x) = abs(abs(abs(abs(x) - 1) - 1) - 1)
+fct_4_str = "f(x) = abs(abs(abs(abs(x) - 1) - 1) - 1)"
+fig_4 = plot_function(fct_4, -10.0, 10.0, 0.01)
 
-f5(x) = 1 / x
-f5d = "f(x) = 1 / x"
-f5p = plot_function(f5, -2.0, 2.0, 0.01)
+fct_5(x) = 1 / x
+fct_5_str = "f(x) = 1 / x"
+fig_5 = plot_function(fct_5, -2.0, 2.0, 0.01)
 
-f6(x) = log(abs(x - 1))
-f6d = "f(x) = log(abs(x - 1))"
-f6p = plot_function(f6, -10.0, 12.0, 0.01)
+fct_6(x) = log(abs(x - 1))
+fct_6_str = "f(x) = log(abs(x - 1))"
+fig_6 = plot_function(fct_6, -10.0, 12.0, 0.01)
 
-plot(f1p, f2p, f3p, f4p, f5p, f6p, layout=6, title=[f1d f2d f3d f4d f5d f6d],
+plot(fig_1, fig_2, fig_3, fig_4, fig_5, fig_6, layout=6, title=[fct_1_str fct_2_str fct_3_str fct_4_str fct_5_str fct_6_str],
     titlefont=font(8), legend=false)
